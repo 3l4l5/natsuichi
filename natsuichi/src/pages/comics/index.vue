f99598b17cb03042355d8363356a67f9a5baa585
Ryusei Ohkura <ohkura.exe@gmail.com>
2023-10-07 13:12:04 +0900

    漫画一覧に日付を出すように修正

--- natsuichi/src/pages/comics/index.vue
+++ natsuichi/src/pages/comics/index.vue
@@ -1,45 +1,50 @@
 <script setup lang="ts">
-import { ref, computed }  from 'vue'
+import { ref, computed } from 'vue'
 const comicList = [
   {
-    preview: "https://pub-d7468921a5ea45d1a7ca87426b5beb75.r2.dev/1/0.webp",
-    title: "8ビートのハイハット",
-    description: "",
-    publishedAt: new Date("2020-01-01"),
+    preview: 'https://pub-d7468921a5ea45d1a7ca87426b5beb75.r2.dev/1/0.webp',
+    title: '8ビートのハイハット',
+    description: '',
+    publishedAt: new Date('2022-07-31'),
     pages: 40,
-    id: "1",
+    id: '1',
+    tags: []
   },
   {
-    preview: "https://pub-d7468921a5ea45d1a7ca87426b5beb75.r2.dev/2/0.webp",
-    title: "いいやつなんだろうけどさ",
-    description: "",
-    publishedAt: new Date("2020-01-01"),
+    preview: 'https://pub-d7468921a5ea45d1a7ca87426b5beb75.r2.dev/2/0.webp',
+    title: 'いいやつなんだろうけどさ',
+    description: '',
+    publishedAt: new Date('2022-09-15'),
     pages: 32,
-    id: "2",
+    id: '2',
+    tags: []
   },
   {
     preview: `https://pub-d7468921a5ea45d1a7ca87426b5beb75.r2.dev/3/0.webp`,
-    title: "全部捨てなきゃ思い出す",
-    description: "メルカリのモノガタリ大賞に応募した作品です。\n何かを思い出すきっかけって、匂いとか、音楽とか、モノとか、そういう身近にあるものだったりするよなと思い描きました。",
-    publishedAt: new Date("2021-09-09"),
+    title: '全部捨てなきゃ思い出す',
+    description:
+      'メルカリのモノガタリ大賞に応募した作品です。\n何かを思い出すきっかけって、匂いとか、音楽とか、モノとか、そういう身近にあるものだったりするよなと思い描きました。',
+    publishedAt: new Date('2021-09-09'),
     pages: 3,
-    id: "3",
+    id: '3',
+    tags: []
   },
   {
-    preview: "https://pub-d7468921a5ea45d1a7ca87426b5beb75.r2.dev/0/0.webp",
-    title: "",
-    description: "",
-    publishedAt: new Date("2999-01-01"),
+    preview: 'https://pub-d7468921a5ea45d1a7ca87426b5beb75.r2.dev/0/0.webp',
+    title: '',
+    description: '',
+    publishedAt: new Date('2999-01-01'),
     pages: 0,
-    id: "#",
+    id: '#',
+    tags: []
   }
 ]
 const hoverFlag = ref(false)
 const mouseOverAction = computed(() => {
-    hoverFlag.value = true
+  hoverFlag.value = true
 })
 const mouseLeaveAction = computed(() => {
-    hoverFlag.value = false
+  hoverFlag.value = false
 })
 </script>
 
@@ -51,8 +56,11 @@ const mouseLeaveAction = computed(() => {
           <div class="comic_preview">
             <div class="comic_preview_rectangle">
               <img class="comic_preview_image" :src="image.preview" />
-              <p class="comicTitle">{{ image.title }}</p>
-              <p class="pageCount" v-if="image.pages">{{ image.pages }}ページ</p>
+              <div class="comic_info" v-if="image.title">
+                <p class="comicTitle">{{ image.title }}</p>
+                <p class="publishedAt">{{ image.publishedAt.toLocaleDateString() }}</p>
+                <p class="pageCount">{{ image.pages }}ページ</p>
+              </div>
             </div>
           </div>
         </a>
@@ -69,14 +77,13 @@ const mouseLeaveAction = computed(() => {
   grid-template-columns: repeat(auto-fit, 200px);
 }
 
-@media(max-width: 400px){
-    .preview_container {
+@media (max-width: 400px) {
+  .preview_container {
     display: grid;
     gap: 10px;
     grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
   }
 }
-
 
 img {
   width: 50%;
@@ -91,7 +98,7 @@ img {
 }
 .comicTitle {
   text-align: center;
-  color:black;
+  color: black;
   padding-top: 2px;
   padding-bottom: 0px;
   margin-top: 0px;
@@ -100,7 +107,15 @@ img {
 }
 .pageCount {
   text-align: center;
-  color:rgb(63, 63, 63);
+  color: rgb(63, 63, 63);
+  padding: 0px;
+  margin: 0px;
+  font-size: 80%;
+}
+
+.publishedAt {
+  text-align: center;
+  color: rgb(63, 63, 63);
   padding: 0px;
   margin: 0px;
   font-size: 80%;
