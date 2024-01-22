3c6acc2cb583603aa35a900053ab7f2f3d45bf05
Ryusei Ohkura <ohkura.exe@gmail.com>
2024-01-22 21:53:17 +0900

    ホーム画面修正

--- natsuichi/src/pages/home/index.vue
+++ natsuichi/src/pages/home/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
+    <h1>赤城ナツイチです！！！！！！！！</h1>
+    <p>📚 描いたまんがは<a href="/comics">こちら</a>。</p>
+    <p>✒️お仕事の連絡などは<a href="/work">こちら</a>。</p>
+    <div class="news">
+      <h3>ニュース</h3>
+      <ul>
+        <li>2024/01/22 <a href="/comics">まんがページ</a>にさよならバードランドを追加しました</li>
+      </ul>
+    </div>
     <img src="../../assets/top_image.webp" />
-    <p>赤城ナツイチです！！！！！！！！</p>
   </div>
 </template>
 
@@ -11,8 +19,18 @@ img {
   height: auto;
   pointer-events: none;
 }
+
+h1 {
+  margin-bottom: 30px;
+  margin-top: 20px;
+}
+.news {
+  border: 1px solid #000000;
+  padding:  0 20px;
+  margin: 30px 5px;
+}
 p {
-  font-size: 2rem;
+  font-size: 1.3rem;
   margin: 10px;
   padding: 0;
   color: #000000;
