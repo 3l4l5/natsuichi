ceb9aab5a77dccb3e21e4db197a2e7a5f0f22d84
Ryusei Ohkura <ohkura.exe@gmail.com>
2024-01-22 22:03:06 +0900

    お仕事ページの文言修正

--- natsuichi/src/pages/work/index.vue
+++ natsuichi/src/pages/work/index.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="contact_container">
+      <p>お仕事やご連絡は、以下のリンクからお受けさせていただいております</p>
       <ui class="list">
         <li><a href="https://twitter.com/DDDD__akagi" target="_blank" class="workLink">twitter</a></li>
         <li><a href="https://coconala.com/users/1767934" class="workLink">ココナラ</a></li>
-        <li><a href="https://skeb.jp/@DDDD__akagi" class="workLink">Skeb</a></li>
         <li><a href="https://www.pixiv.net/users/65997288" target="_blank" class="workLink">Pixiv</a></li>
         <li><a href="mailto:akagihajimee@gmail.com" target="_blank" class="workLink">メール</a></li>
       </ui>
@@ -23,7 +23,7 @@
     display: flex;
     align-items: center;
     gap: 0 10px;
-    padding: .4em;
+    padding: 1em 0em;
 }
 
 .list li::before {
@@ -36,6 +36,7 @@
 }
 
 a.workLink {
-  color: #000;
+  /* color: #000; */
+  text-decoration:underline solid rgb(0, 60, 255);
 }
 </style>
