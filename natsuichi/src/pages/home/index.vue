0dd65bc0989229477020abc63a73bc4dd4b9e50f
Ryusei Ohkura <ohkura.exe@gmail.com>
2024-12-01 21:18:04 +0900

    update link

--- natsuichi/src/pages/home/index.vue
+++ natsuichi/src/pages/home/index.vue
@@ -8,8 +8,7 @@
       <ul>
         <li>
           2024/11/19
-          <a
-            href="http://www.dokidokivisual.com/magazine/max/book/index.php?mid=931#:~:text=%E3%81%A8%E9%96%A2%E8%A5%BF%E5%BC%81%E3%80%8F-,%E8%B5%A4%E5%9F%8E%E3%83%8A%E3%83%84%E3%82%A4%E3%83%81%E3%80%8E%E3%81%8D%E3%81%BF%E3%81%A8%E3%83%9C%E3%83%89%E3%82%B2%E3%81%8C%E4%BD%9C%E3%82%8A%E3%81%9F%E3%81%84%EF%BC%81%E3%80%8F,-O%E4%BB%AE%E5%90%8D%E3%81%A0"
+          <a href="https://x.com/mangatimekirara/status/1858511262201905188"
             >まんがタイムきららMAX 2025年1月号</a
           >(2024年11月発売)にて、執筆した漫画「きみとボドゲが作りたい！」1話 2話が掲載されました。
         </li>
