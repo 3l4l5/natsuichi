f7a634e756ae37586e89af8d27c57efafdbb1e2b
Ryusei Ohkura <ohkura.exe@gmail.com>
2025-02-11 22:16:50 +0900

    連絡ページを更新

--- natsuichi/src/pages/work/index.vue
+++ natsuichi/src/pages/work/index.vue
@@ -1,13 +1,19 @@
 <template>
-    <div class="contact_container">
-      <p>お仕事やご連絡は、以下のリンクからお受けさせていただいております</p>
-      <ui class="list">
-        <li><a href="https://twitter.com/DDDD__akagi" target="_blank" class="workLink">twitter</a></li>
-        <li><a href="https://coconala.com/users/1767934" class="workLink">ココナラ</a></li>
-        <li><a href="https://www.pixiv.net/users/65997288" target="_blank" class="workLink">Pixiv</a></li>
-        <li><a href="mailto:akagihajimee@gmail.com" target="_blank" class="workLink">メール</a></li>
-      </ui>
+  <div class="p-2 grid grid-cols-1 justify-items-center gap-4">
+    <p class="text-lg">SNSなど</p>
+    <div class="grid-cols-2 grid">
+      <a href="https://twitter.com/DDDD__akagi" target="_blank">
+        <div class="border rounded-sm p-2 m-2 w-30 flex justify-center">twitter</div></a
+      >
+      <!-- <div class="border rounded-sm p-2 m-2 w-30"><a href="https://coconala.com/users/1767934" class="">ココナラ</a></div> -->
+      <a href="https://www.pixiv.net/users/65997288" target="_blank"
+        ><div class="border rounded-sm p-2 m-2 w-30 flex justify-center">Pixiv</div></a
+      >
+      <a href="mailto:akagihajimee@gmail.com" target="_blank"
+        ><div class="border rounded-sm p-2 m-2 w-30 flex justify-center">メール</div></a
+      >
     </div>
+  </div>
 </template>
 
 <style>
@@ -16,27 +22,22 @@
 }
 
 .list {
-    list-style-type: none;
+  list-style-type: none;
 }
 
 .list li {
-    display: flex;
-    align-items: center;
-    gap: 0 10px;
-    padding: 1em 0em;
+  display: flex;
+  align-items: center;
+  gap: 0 10px;
+  padding: 1em 0em;
 }
 
 .list li::before {
-    transform: rotate(-45deg);
-    width: .4em;
-    height: .4em;
-    border-bottom: 3px solid #000000;
-    border-right: 3px solid #000000;
-    content: '';
-}
-
-a.workLink {
-  /* color: #000; */
-  text-decoration:underline solid rgb(0, 60, 255);
+  transform: rotate(-45deg);
+  width: 0.4em;
+  height: 0.4em;
+  border-bottom: 3px solid #000000;
+  border-right: 3px solid #000000;
+  content: '';
 }
 </style>
